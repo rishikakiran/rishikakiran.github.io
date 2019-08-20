caa42be79b4fcb2b56824e54e38a82a4d63d9864
737282 <737282@cognizant.com>
2019-08-20 11:40:44 +0530

    fixed footer

---
+++ scrollspy.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function(){
+  // the JavaScript you want to run after the page loads
+  'use strict';
+
+  var section = document.getElementsByTagName("section");
+  var sections = {};
+  var i = 0;
+
+  Array.prototype.forEach.call(section, function(e) {
+    sections[e.id] = e.offsetTop;
+  });
+
+  window.onscroll = function() {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (i in sections) {
+      if (sections[i] <= scrollPosition) {
+        document.querySelector('.active').setAttribute('class', ' ');
+        document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+      }
+    }
+  };
+}, false);
